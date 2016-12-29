993af16677e1569269c7b8f02060751b7332dba6
Tim Reichelt <treichelt6@gmail.com>
2016-12-29 12:51:08 +0100

    first commit

---
+++ process_geotiff.py
@@ -0,0 +1,13 @@
+"""Module to handle processing the raw GeoTIFF data from satellite imagery."""
+
+def read_geotiff(file_name, satellite_type="sentinel-2"):
+    pass
+
+def get_bounds(raster_dataset):
+    pass
+
+def create_tiles(raster_dataset, tile_size):
+    pass
+
+def create_bitmap(tile):
+    pass
